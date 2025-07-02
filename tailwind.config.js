ba8e60c91a0b69892116600e8061e842cd24a263
Fakhrul Hidayat <fakhrulhidayat33@gmail.com>
2025-07-02 16:16:31 +0700

    starting point

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        desktop: '1024px', // same as 'lg' if you want
+      }
+    }
+  },
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
